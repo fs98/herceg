8eeb98a16bafe28abc47fdc65c65ae99ac9cf0a8
Fata <sefer.fata@gmail.com>
2021-06-17 15:29:10 +0200

    New design

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,48 @@ require('./bootstrap');
 
 // Import Sweet Alert
 import Swal from 'sweetalert2';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'; 
+
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// Custom JS
+window.onload = function() {
+  
+  // Swiper Init
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 3,
+    spaceBetween: 10,
+    // init: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 40,
+      },
+    },
+  });
+  
+  // Aos Init
+  AOS.init({
+    once: true,
+  });
+}
