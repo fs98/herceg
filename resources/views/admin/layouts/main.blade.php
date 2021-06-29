e51814d78e52c9b0fad8401607f58e81a25fdb7b
Fata <sefer.fata@gmail.com>
2021-06-29 13:16:28 +0200

    Minor fixes!

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -290,16 +290,16 @@
       <div class="modal-dialog" role="document">
           <div class="modal-content">
               <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                  <h5 class="modal-title" id="exampleModalLabel">{{ __('Želite se odjaviti?') }}</h5>
                   <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">×</span>
                   </button>
               </div>
-              <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+              <div class="modal-body">{{ __('Kliknite na tipku "Odjavi se" ako želite nastaviti sa odjavom.') }}</div>
               <div class="modal-footer">
-                  <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                  <button class="btn btn-secondary" type="button" data-dismiss="modal">{{ __('Odustani') }}</button>
                   <a class="btn btn-success" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
+                      {{ __('Odjavi se') }}
                   </a>
 
                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
